1103e1dff7dce286ca89f954b894ffb24277270b
ibrahim <ibrahimh.cadirci@gmail.com>
2019-11-04 01:07:50 +0300

    kütüphane ve test dosyalarında değişiklik yapıldı.

--- src/Films.php
+++ src/Films.php
@@ -3,24 +3,48 @@ namespace ibrahimhcadirci\films;
 
 class Films {
 
-    protected $films    = [];
+   protected $films    = [],
+             $categorys = [];
    public function __construct(){
-       
+        $this->vision_get();
    }
 
    public function vision_get() {
-        $curl = curl_init();// curl işlemini başlatıyoruz
-        curl_setopt($curl,CURLOPT_URL,"http://sinema.mynet.com/vizyondaki-filmler");// linkimizi belirtiyoruz
-        curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);// veritransferini etkinleştiriyoruz
-        curl_setopt($curl,CURLOPT_FOLLOWLOCATION,1); // linki takip etmesini sağlıyoruz
-        $sonuc = curl_exec($curl); // ve sonucu bir değişkene atıyoruz
-        preg_match_all('@<div class="vizyonImg">(.*?)</div>@si',$sonuc,$new); /* preg_match_all ile de sadece istdiğimiz kısımları seçiyoruz son olarak döngüye sokup yazdırıyoruz..*/ 
+        $curl = curl_init();
+        curl_setopt($curl,CURLOPT_URL,"http://sinema.mynet.com/vizyondaki-filmler");
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($curl,CURLOPT_FOLLOWLOCATION,1);
+        $sonuc = curl_exec($curl);
+        preg_match_all('@<div class="vizyonImg">(.*?)</div>@si',$sonuc,$new); 
         preg_match_all('@<ul class="vizyonInfo clr">(.*?)</ul>@si',$sonuc,$info);
         $new = $new[0];
         $info = $info[0];
         for($i=0; $i < count($info);$i++){
-            array_push($this->films, ["images"  => $new[$i], "info"     => $info[$i]]);
+            preg_match_all('/<a href="(.*?)" /',$new[$i],$link); 
+            preg_match_all('/<img src="(.*?)" /',$new[$i],$image); 
+            preg_match_all('/<span itemprop="name">(.*)<\/span>/',$new[$i],$name); 
+            preg_match_all('@<li><span(.*?)</span>(.*?)</li>@si',$info[$i],$info_data); 
+            $type       = isset($info_data[2][3]) ? $info_data[2][3] : 'Bilinmiyor' ;
+            $this->films[$i]        = ["name"        => $name[1][0],"link"   => $link[1][0], "image"  => $image[1][0], 
+                "year"  => $info_data[2][0],
+                "display_date"  => $info_data[2][1],
+                "point"  => $info_data[2][2],
+                "film_type"  => $type
+            ];
+            array_push($this->categorys, $type);
+
         }
-        return $this->films;
+   }
+
+   public function get($cache = null){
+       return $this->films;
+   }
+
+   public function categorys(){
+        return $this->categorys;
+   }
+
+   public function random(){
+       return $this->films[rand(0,(count($this->films) - 1))];
    }
 }
--- tests/index.php
+++ tests/index.php
@@ -4,7 +4,9 @@ use \ibrahimhcadirci\films\Films;
 
 $films      = new Films();
 
-$films      = $films->vision_get();
+$random      = $films->random();
+$films_data      = $films->get();
+$categorys      = $films->categorys();
 ?>
 
 <!DOCTYPE html>
@@ -15,6 +17,26 @@ $films      = $films->vision_get();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
+    * {
+   
+    
+    box-sizing: border-box;
+    margin : 0px ; padding : 0px;
+    
+    }
+    body {
+        background : #ededed;
+        font-family : 'Roboto', sans-serif;
+    font-size:14px;
+    }
+    .films {
+        width: 600px;
+        background: #fff;
+        padding: 15px;
+        margin: 0px auto;
+        box-shadow: 0px 0px 5px #3333332b;
+        border-radius: 3px;
+    }
     .films .vizyonImg {
     border: 2px solid #ccc;
     padding: 15px;
@@ -23,11 +45,7 @@ $films      = $films->vision_get();
     margin: 0px 15px 15px 0px;
 }
 
-* {
-    font-family : 'Roboto', sans-serif;
-    font-size:14px;
-    
-    box-sizing: border-box;}
+
 
 .vizyonImg a {
     color: #333;
@@ -36,15 +54,82 @@ $films      = $films->vision_get();
     text-decoration: none;
     text-align: center;
 }
+.films h2.title  {
+    display: block;
+    text-align:center;
+}
+.films h2.title a{
+    font-size: 12px;
+    color : #333;
+    text-decoration: none;
+    border-radius: 5px;
+    background: #ededed;
+    padding: 3px;
+    border: 1px solid #ccc;
+}
+.films .random-film {
+    background: #ef665d;
+    padding: 14px;
+    margin: 15px -15px;
+    text-align: center;
+    color: #fff;
+    font-weight: 300;
+    font-size: 18px;
+}
+ul.vision-films li {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 15px;
+}
+ul.vision-films li .col{
+    flex-grow : 1;
+}
+ul.vision-films li .col:nth-child(1) {
+    flex-grow : 10;
+}
+ul.vision-films li span {
+    font-size: 13px;
+    margin: 5px;
+    display: block;
+    border-bottom: 1px solid #ededed;
+}
+ul.vision-films li p {
+    font-weight: bold;
+    color: #F44336;
+    text-align: center;
+}
+h3.title {
+    padding: 15px 0px;
+    border-bottom: 1px dashed;
+    margin-bottom: 15px;
+}
     </style>
 </head>
 <body>
 <div class="films">
-
-    <?php foreach($films as $row) {?>
-            <?=$row['images']?>
-
-<?php }?>
+    <h2 class="title">PHP ile vizyondaki filmeleri çekme <a href="https://github.com/ibrahimhcadirci/php-vizyondaki-filmler" target="_blank">Github</a></h2>
+    <div class="random-film">
+    <p>Random film : <?=$random['name']?></p>
+    </div>
+    <h3 class="title">Vizyondaki Filmler</h3>
+    <ul class="vision-films">
+        <?php  foreach($films_data  as $row) {
+        ?>
+            <li>
+                <div class="col">
+                    <p><?=$row['name']?></p> <br> 
+                    <span>Çıkış Tarihi : <?=$row['year']?></span>
+                    <span>Aldığı Puan : <?=$row['point']?></span>
+                    <span>Gösterime Girdiği Tarih: <?=$row['display_date']?></span>
+                    <span>Kategori: <?=$row['film_type']?></span>
+                </div>
+                <div class="col">
+                    <img src="<?=$row['image']?>" alt="<?=$row['title']?>"/>
+                </div>
+            </li>
+        <?php
+        }?>
+    </ul>
 </div>
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="js/jquery.matchHeight-min.js"></script>
